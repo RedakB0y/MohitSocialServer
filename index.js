4719fd21d1553e20efef127ae88107eb8f5e8748
unknown <redakgaming@gmail.com>
2023-02-11 00:31:59 +0530

    socketIOServer

---
+++ index.js
@@ -0,0 +1,45 @@
+const io = require("socket.io")(8900, {
+    cors: {
+        origin: "http://localhost:3000",
+    }
+});
+
+let users = [];
+
+const adduser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) && users.push({ userId, socketId })
+}
+const removeuser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId);
+}
+
+const getuser = (userId) => {
+    return users.find(user => user.userId === userId)
+}
+
+io.on("connection", (socket) => {
+    console.log("user connected");
+    socket.on("addUser", userId => {
+        adduser(userId, socket.id);
+        io.emit("getUsers", users);
+    });
+    // send and get message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getuser(receiverId);
+        if (user) {
+            io.to(user.socketId).emit("getMessage", {
+                senderId,
+                text,
+            });
+        }
+
+    });
+
+    socket.on("disconnect", () => {
+        console.log("user Disconnected");
+        removeuser(socket.id);
+        io.emit("getUsers", users);
+    });
+
+
+});
